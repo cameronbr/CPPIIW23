53b0f14e40a401f18c450baf6a4c38f9df3732bb
jrkave <110064455+jrkave@users.noreply.github.com>
2023-01-26 05:41:01 +0000

    first commit

---
+++ header.h
@@ -0,0 +1,19 @@
+/*
+*   Filename: header.h
+*   Created on: Jan. 26, 2023
+*   Authors: Veenkamp, James, Brooks
+*/
+
+#include <iostream>
+
+#ifndef HEADER_H_
+#define HEADER_H_
+using namespace std;
+
+// Declare variables
+
+// Declare functions
+
+// Declare classes
+
+#endif /* SHAPES_H_ */
